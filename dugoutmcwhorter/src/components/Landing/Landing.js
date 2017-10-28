dab7db80efef689b1085137f9419ef89d96c1656
randyh <randyh@qualtrics.com>
2017-10-28 00:20:23 -0600

    nav bar

--- dugoutmcwhorter/src/components/Landing/Landing.js
+++ dugoutmcwhorter/src/components/Landing/Landing.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "./Landing.css";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import Nav from "../../components/Nav/Nav.js";
-import { getUserInfo } from "../../ducks/reducer";
+import React, { Component } from 'react';
+import './Landing.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Nav from '../../components/Nav/Nav.js';
+import { getUserInfo } from '../../ducks/reducer';
 
 class Landing extends Component {
   componentDidMount() {
@@ -14,8 +14,6 @@ class Landing extends Component {
     return (
       <div className="App">
         <Nav />
-        <p>Text here</p>
-        <p>Text also here</p>
       </div>
     );
   }
--- dugoutmcwhorter/src/components/Nav/Nav.js
+++ dugoutmcwhorter/src/components/Nav/Nav.js
@@ -1,77 +1,77 @@
-import React, { Component } from "react";
-import "./Nav.css";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import './Nav.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import {
   updateDatabaseType,
   clearCSR,
   updateProducts
-} from "../../ducks/reducer";
+} from '../../ducks/reducer';
 
 class Nav extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/">
-          <div className="titleCon ">The DugOut</div>
-        </Link>
-        <div className="navCon">
-          <Link to="/about">
-            <div>About</div>
-          </Link>
-          <Link to="/contact">
-            <div>Contact</div>
-          </Link>
-          <Link to="/database">
-            <div
-              onClick={() => {
-                this.props.updateDatabaseType(1);
-                console.log(this.props.user);
-                this.props.clearCSR();
-              }}
-            >
-              Cards
-            </div>
-          </Link>
-          <Link to="/database">
-            <div
-              onClick={() => {
-                this.props.updateDatabaseType(2);
-                console.log(this.props.user);
-                this.props.clearCSR();
-                this.props.updateProducts();
-              }}
-            >
-              Miscellaneous
-            </div>
-          </Link>
-          {this.props.user === 0 ? (
-            <a href={process.env.REACT_APP_LOGIN} className="linkCon">
-              <button>
-                <div style={{ padding: 25 }} className="loginText">
-                  Login/ Register
-                </div>
-              </button>
-            </a>
-          ) : (
-            <a href={process.env.REACT_APP_LOGOUT} className="linkCon">
-              <button>
+        <ul>
+          <li>
+            <Link to="/">
+              <div className="titleCon ">The DugOut</div>
+            </Link>
+          </li>
+          <li />
+          <li>
+            <Link to="/database">
+              <div
+                onClick={() => {
+                  this.props.updateDatabaseType(1);
+                  console.log(this.props.user);
+                  this.props.clearCSR();
+                }}
+              >
+                Cards
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/database">
+              <div
+                onClick={() => {
+                  this.props.updateDatabaseType(2);
+                  this.props.clearCSR();
+                  this.props.updateProducts();
+                }}
+              >
+                Miscellaneous
+              </div>
+            </Link>
+          </li>
+          <li className="logout">
+            {this.props.user === 0 ? (
+              <a href={process.env.REACT_APP_LOGIN} className="linkCon">
+                <div className="loginText">Login/ Register</div>
+              </a>
+            ) : (
+              <a href={process.env.REACT_APP_LOGOUT} className="linkCon">
                 <div className="loginText">Logout</div>
-              </button>
-            </a>
-          )}
-        </div>
-        <div className="footCon">
-          <Link to="/">
-            <div>Home</div>
-          </Link>
-          <Link to="/about">
-            <div>About</div>
-          </Link>
-          <Link to="/contact">
-            <div>Contact</div>
-          </Link>
+              </a>
+            )}
+          </li>
+        </ul>
+        <div className="footer">
+          <ul>
+            <li />
+            <li>
+              <Link to="/about">
+                <div>About</div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <div>Contact</div>
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     );
